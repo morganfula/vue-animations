16a38fb527225213cd36e7f6e4790bd82360db38
morganfula <morgan.fula@me.com>
2021-02-07 19:21:10 +0100

    animation classname

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
     <button @click="animateBlock">Animate</button>
   </div>
   <div class="container">
-    <transition>
+    <transition name="para">
       <p v-if="parasIsVisible">This is only sometimes visible...</p>
     </transition>
     <button @click="toggleParagraph">Toggle Paragraph</button>
@@ -90,35 +90,37 @@ button:active {
 
 .animate {
   /* transform: translateX(-150px); */
-  animation: slide-fade 0.3s ease-out forwards;
+  animation: slide-scale 0.3s ease-out forwards;
 }
 
-.v-enter-from {
-  opacity: 0;
-  transform: translateY(-20px);
+.para-enter-from {
+  /* opacity: 0;
+  transform: translateY(-20px); */
 }
 
-.v-enter-active {
-  transition: all 0.3s ease-out;
+.para-enter-active {
+  /* transition: all 0.3s ease-out; */
+  animation: slide-scale 0.3s ease-out;
 }
-.v-enter-to {
-  opacity: 1;
-  transform: translateY(0);
-}
-
-.v-leave-from {
-  opacity: 1;
-  transform: translateY(0);
-}
-.v-leave-active {
-  transition: all 0.3s ease-in;
-}
-.v-leave-to {
-  opacity: 0;
-  transform: translateY(20px);
+.para-enter-to {
+  /* opacity: 1;
+  transform: translateY(0); */
 }
 
-@keyframes slide-fade {
+.para-leave-from {
+  /* opacity: 1;
+  transform: translateY(0); */
+}
+.para-leave-active {
+  /* transition: all 0.3s ease-in; */
+  animation: slide-scale 0.3s ease-out;
+}
+.para-leave-to {
+  /* opacity: 0;
+  transform: translateY(20px); */
+}
+
+@keyframes slide-scale {
   0% {
     transform: translateX(0) scale(1);
   }
